cbd7e4d59308e29f61c6fd044a24a74618d10aa6
Katya <katyalolka1994@gmail.com>
2022-09-14 11:48:21 +0300

    first commit

---
+++ a1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+int Multiplication(int a, int b)
+{
+    int multi = 1;
+    int index = 1;
+    while (index <= b)
+    {
+        multi = multi * a;
+        index++;
+    }
+    return multi;
+}
+Console.WriteLine($"{a},{b} -> {Multiplication(a, b)}");
---
+++ a2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int KolChisel (int a)
+{
+  int count = 10;
+  int i = 1;
+  while (a >= count)
+  {
+    count = count*10;
+    i++;
+  }
+  return i;
+}
+int il = KolChisel(a);
+
+int SumChisel (int kol2, int a)
+{
+  int index = 1;
+  int sum = 0;
+  while (index <= kol2)
+  {
+    sum = sum + a % 10;
+    a = a / 10;
+    index++;
+  }
+  return sum;
+}
+Console.WriteLine($"{a} -> {SumChisel(il, a)}");
